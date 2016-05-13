f499219e417a6ffd89c8aa303672e0fbdfa6112a
Blake Merryman <blake.merryman@gmail.com>
2016-05-12 23:30:50 -0500

    Updating copyrights

--- OptionKit/Option.swift
+++ OptionKit/Option.swift
@@ -3,7 +3,7 @@
 //  OptionKit
 //
 //  Created by Blake Merryman on 1/25/15.
-//  Copyright (c) 2015 Blake Merryman. All rights reserved.
+//  Copyright (c) 2016 Blake Merryman. All rights reserved.
 //
 
 import Foundation
--- OptionKit/OptionKit.h
+++ OptionKit/OptionKit.h
@@ -3,7 +3,7 @@
 //  OptionKit
 //
 //  Created by Blake Merryman on 1/29/15.
-//  Copyright (c) 2015 Blake Merryman. All rights reserved.
+//  Copyright (c) 2016 Blake Merryman. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
